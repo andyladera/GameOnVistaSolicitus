e8d5e0b33d2b2eee8b30cac45c8930ee5ab50775
SebastianFuentesAvalos <sf2022073902@virtual.upt.pe>
2025-06-19 04:32:58 -0500

    prueba01

--- Config/database.php
+++ Config/database.php
@@ -1,22 +1,41 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $db_name = "gameon";
-    private $username = "root";
-    private $password = "";
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    private $port;
     private $conn;
+    
+    public function __construct() {
+        // Verificar si estamos en producción (Azure) o desarrollo local
+        if (isset($_ENV['MYSQL_HOST']) || getenv('MYSQL_HOST')) {
+            // Configuración de Railway (producción)
+            $this->host = $_ENV['MYSQL_HOST'] ?? getenv('MYSQL_HOST');
+            $this->db_name = $_ENV['MYSQL_DATABASE'] ?? getenv('MYSQL_DATABASE');
+            $this->username = $_ENV['MYSQL_USER'] ?? getenv('MYSQL_USER');
+            $this->password = $_ENV['MYSQL_PASSWORD'] ?? getenv('MYSQL_PASSWORD');
+            $this->port = $_ENV['MYSQL_PORT'] ?? getenv('MYSQL_PORT') ?? 3306;
+        } else {
+            // Configuración local
+            $this->host = "localhost";
+            $this->db_name = "gameon";
+            $this->username = "root";
+            $this->password = "";
+            $this->port = 3306;
+        }
+    }
+
     public function getConnection() {
         $this->conn = null;
 
         try {
-            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name, $this->port);
             
-            // Verificar la conexión
             if ($this->conn->connect_error) {
                 throw new Exception("Error de conexión: " . $this->conn->connect_error);
             }
             
-            // Establecer el conjunto de caracteres a utf8
             $this->conn->set_charset("utf8");
             
         } catch(Exception $e) {
@@ -26,7 +45,6 @@ class Database {
         return $this->conn;
     }
     
-    // Método para cerrar la conexión
     public function closeConnection() {
         if ($this->conn != null) {
             $this->conn->close();
